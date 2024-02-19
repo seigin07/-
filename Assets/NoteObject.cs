d081fc3ed3033ce1e573260fc3c1c06775bd8ee2
99duck99 <kamdabin@gmail.com>
2024-02-19 19:04:20 +0900

    ver2

--- Assets/NoteObject.cs
+++ Assets/NoteObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NoteObject : MonoBehaviour
 {
+
+    public bool canBePressed;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,29 @@ public class NoteObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(canBePressed)
+            {
+                gameObject.SetActive(false);
+                Debug.Log("hi");
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = false;
+            Destroy(gameObject);
+        }
     }
 }
